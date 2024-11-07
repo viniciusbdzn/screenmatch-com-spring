f7c23bf46496824c869a95354f2cde89680ec93a
viniciusbdzn <viniciusbdzn@gmail.com>
2024-11-06 23:27:14 -0300

    enviando codigo da aula sobre streams, lambda e collections

--- src/main/java/br/com/alura/screenmatch/model/DadosEpisodio.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosEpisodio.java
@@ -1,4 +1,12 @@
 package br.com.alura.screenmatch.model;
 
-public record DadosEpisodio() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosEpisodio(@JsonAlias("Title") String titulo,
+                            @JsonAlias("Episode") Integer numero,
+                            @JsonAlias("imdbRating") String avaliacao,
+                            @JsonAlias("Released") String dataLancamento) {
+
 }
--- src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
+++ src/main/java/br/com/alura/screenmatch/model/DadosTemporada.java
@@ -1,4 +1,11 @@
 package br.com.alura.screenmatch.model;
 
-public record DadosTemporada() {
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DadosTemporada(@JsonAlias("Season") Integer numero,
+                             @JsonAlias("Episodes") List<DadosEpisodio> episodios) {
 }
